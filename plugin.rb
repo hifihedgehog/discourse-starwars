23d378b51d48e86b5fca155abb43cd77746a6a12
hifihedgehog <16614343+hifihedgehog@users.noreply.github.com>
2020-04-17 13:50:19 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,53 +1,53 @@
-# name: discourse-nationalflags
-# about: Display National Flags from User's home countries.
+# name: discourse-starwars
+# about: Display Star Wars locales from User's selected locations.
 # version: 2.0
-# authors: Neil Ebrey <neil.ebrey@gmail.com>, Rob Barrow <merefield@gmail.com>
-# url: https://github.com/Ebsy/discourse-nationalflags
+# authors: David Forbush
+# url: https://github.com/hifihedgehog/discourse-starwars
 
-enabled_site_setting :nationalflag_enabled
+enabled_site_setting :starwars_enabled
 
-PLUGIN_NAME = "discourse-nationalflags"
+PLUGIN_NAME = "discourse-starwars"
 
-DiscoursePluginRegistry.serialized_current_user_fields << "nationalflag_iso"
+DiscoursePluginRegistry.serialized_current_user_fields << "starwars_iso"
 
 after_initialize do
 
-  module ::DiscourseNationalFlags
+  module ::DiscourseStarWars
     class Engine < ::Rails::Engine
-      engine_name "discourse_national_flags"
-      isolate_namespace DiscourseNationalFlags
+      engine_name "discourse_stars"
+      isolate_namespace DiscourseStarWars
     end
   end
 
-  load File.expand_path('../lib/flags.rb', __FILE__)
+  load File.expand_path('../lib/locales.rb', __FILE__)
 
   Discourse::Application.routes.append do
-    mount ::DiscourseNationalFlags::Engine, at: 'natflags'
+    mount ::DiscourseStarWars::Engine, at: 'starwars'
   end
 
-  load File.expand_path('../controllers/flags.rb', __FILE__)
+  load File.expand_path('../controllers/starwars.rb', __FILE__)
 
-  ::DiscourseNationalFlags::Engine.routes.draw do
-    get "/flags" => "flags#flags"
+  ::DiscourseStarWars::Engine.routes.draw do
+    get "/starwars" => "starwars#starwars"
   end
 
   public_user_custom_fields_setting = SiteSetting.public_user_custom_fields
   if public_user_custom_fields_setting.empty?
-    SiteSetting.set("public_user_custom_fields", "nationalflag_iso")
-  elsif public_user_custom_fields_setting !~ /nationalflag_iso/
+    SiteSetting.set("public_user_custom_fields", "starwars_iso")
+  elsif public_user_custom_fields_setting !~ /starwars_iso/
     SiteSetting.set(
       "public_user_custom_fields",
-      [SiteSetting.public_user_custom_fields, "nationalflag_iso"].join("|")
+      [SiteSetting.public_user_custom_fields, "starwars_iso"].join("|")
     )
   end
 
-  User.register_custom_field_type('nationalflag_iso', :text)
+  User.register_custom_field_type('starwars_iso', :text)
 
-  register_editable_user_custom_field :nationalflag_iso if defined? register_editable_user_custom_field
+  register_editable_user_custom_field :starwars_iso if defined? register_editable_user_custom_field
   
-  if SiteSetting.nationalflag_enabled then
+  if SiteSetting.starwars_enabled then
     add_to_serializer(:post, :user_signature, false) {
-      object.user.custom_fields['nationalflag_iso']
+      object.user.custom_fields['starwars_iso']
     }
 
     # I guess this should be the default @ discourse. PR maybe?
@@ -61,21 +61,21 @@ after_initialize do
   end
 end
 
-register_asset "javascripts/discourse/templates/connectors/user-custom-preferences/user-nationalflags-preferences.hbs"
+register_asset "javascripts/discourse/templates/connectors/user-custom-preferences/user-starwars-preferences.hbs"
 register_asset "javascripts/discourse/templates/connectors/user-profile-primary/show-user-card.hbs"
-register_asset "stylesheets/nationalflags.scss"
+register_asset "stylesheets/starwars.scss"
 
 DiscourseEvent.on(:custom_wizard_ready) do
   if defined?(CustomWizard) == 'constant' && CustomWizard.class == Module
-    CustomWizard::Field.add_assets('national-flag', 'discourse-nationalflags', ['components', 'templates'])
+    CustomWizard::Field.add_assets('starwars', 'discourse-starwars', ['components', 'templates'])
 
-    CustomWizard::Builder.add_field_validator('national-flag') do |field, updater, step_template|
+    CustomWizard::Builder.add_field_validator('starwars') do |field, updater, step_template|
       if step_template['actions'].present?
         step_template['actions'].each do |a|
           if a['type'] === 'update_profile'
             a['profile_updates'].each do |pu|
-              if pu['key'] === field['id'] && pu['value_custom'] === 'nationalflag_iso'
-                updater.fields[field['id']] = updater.fields[field['id']]['nationalflag_iso']
+              if pu['key'] === field['id'] && pu['value_custom'] === 'starwars_iso'
+                updater.fields[field['id']] = updater.fields[field['id']]['starwars_iso']
               end
             end
           end
